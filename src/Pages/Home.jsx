987bcab91ca6672a25d6266bd6d7f38511c95bc9
Arthur Fernandez <arthur@snooget.com>
2023-07-22 13:29:52 -0700

    added video to the section 1 homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>Home</div>
+    <> {/* Return container holder */}
+      <div> {/* Parent div of section 1 with a video and src that links to company visual overview */}
+        <div>
+          <video src="https://cdn.sanity.io/files/z5s3oquj/production/b7f8f4a89e564305b8c810113d7bf71d445b1419.mp4" loop></video>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,25 +2,27 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDropdown = () => setIsOpen(!isOpen);
 
   return (
     <>
-      <div className="container mx-auto"> {/*Parent container*/}
-        <div> {/* Parent container of the ul and li */}
+      <div className="container mx-auto"> 
+        <div className="bg-white">
           <ul className='flex justify-between'>
-            <li className=''>
+            <li className='border-solid border-white cursor-pointer hover:fill-gray-200'>
               <Link to="/">Arcadius WP</Link>
             </li>
-            <li className='' >
+            <li className='cursor-pointer'>
               <Link to="/hive">H.I.V.E Program</Link>
             </li>
-            <li className=''>
+            <li className='cursor-pointer'>
               <Link to="/air-security">Air Security</Link>
             </li>
-            <li className=''>
+            <li className='cursor-pointer'>
               <Link to="/maritime-security">Maritime Security</Link>
             </li>
-            <li className=''>
+            <li className='cursor-pointer'>
               <Link to="hiring">We're Hiring</Link> 
             </li>           
           </ul>
