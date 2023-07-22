f197e9a5122de578625c14b695e0564a9d397a71
Arthur Fernandez <arthur@snooget.com>
2023-07-22 11:52:54 -0700

    Changes router-dom structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import './App.css';
-import Routes from './Routes';
 import Header from './components/Header';
 import ProductListing from './ProductListing';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Product from './Pages/Product';
+import Hive from './components/Hive';
+import AirSecurity from './components/AirSecurity';
+import MaritimeSecurity from './components/MaritimeSecurity';
+import Hiring from './Pages/Hiring';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <>
-      <Header />
-      <ProductListing />
-      <Routes />
+      <Router>
+        <Header />
+        <ProductListing />
+        <Routes>
+          <Route path="/" element={<Home />}  />
+          <Route path="/product" element={<Product />} />
+          <Route path="/hive" element={<Hive />}/>
+          <Route path="/air-security" element={<AirSecurity />}/>
+          <Route path="/maritime-security" element={<MaritimeSecurity />}/>
+          <Route path="/hiring" element={<Hiring />}/>
+        </Routes>
+      </Router>
     </>
   );
 };
