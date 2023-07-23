66e3dd85d05f774497a1bbddf1a3e16187ea02bd
Arthur Fernandez <arthur@snooget.com>
2023-07-23 12:15:17 -0700

    Added new border to hiring <li>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,33 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  }
+
+  const mobileMenu = isMobileMenuOpen ? (
+    <div className="absolute top-0 right-0 h-screen w-64 bg-gray-100 p-8 lg:hidden">
+      <button onClick={toggleMobileMenu} className="mb-8">Close Menu</button>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/hive">H.I.V.E Program</Link></li>
+        <li><Link to="/air-security">Air Security</Link></li>
+        <li><Link to="/maritime-security">Maritime Security</Link></li>
+        <li><Link to="hiring">We're Hiring</Link></li>
+      </ul>
+    </div>
+  ) : null;
 
   return (
     <div className="container mx-auto">
       <div className="text-white">
+        <button onClick={toggleMobileMenu} className="md:hidden">Menu</button>
+        {mobileMenu}
         <ul className='relative z-10 flex justify-between bg-transparent border-b-2 border-r-2 border-l-2'>
           <li className="text-lg border-r-2 pr-10 py-10">
-            <Link to="/">Anduril WP</Link>
+            <Link to="/">Anduril</Link>
           </li>
           <li className="relative group"
             onMouseEnter={() => setIsOpen(true)}
@@ -36,7 +56,7 @@ export default function Header() {
             <Link to="/maritime-security" className="inline-block py-10">Maritime Security</Link>
           </li>
           <li>
-            <Link to="hiring" className="inline-block py-10">We're Hiring</Link> 
+            <Link to="hiring" className="inline-block py-10 border-l-2">We're Hiring</Link> 
           </li>           
         </ul>
       </div>
