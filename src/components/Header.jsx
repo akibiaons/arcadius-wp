c931ca67faa425c1b7caa0ec3358701e5c7e18bd
Arthur Fernandez <arthur@snooget.com>
2023-07-22 17:44:03 -0700

    Fixed onMouseLeave function logic error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,50 +1,45 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <>
-      <div className="container mx-auto"> 
-        <div className="bg-white">
-          {/* The <ul> below containing all the links and submenus of the navbar */}
-          <ul className='flex justify-between'>
-            <li className='border-solid border-white cursor-pointer hover:fill-gray-200'>
-              <Link to="/">Arcadius WP</Link>
-            </li>
-            <li 
-              className='cursor-pointer relative group'
-              onMouseEnter={() => setIsOpen(true)}
-              onMouseLeave={() => setIsOpen(false)}
-            >
-              <Link to="/hive">H.I.V.E Program</Link>
-              {isOpen && (
-                <ul className="absolute left-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none z-50">
-                  <li className="px-4 py-3">
-                    <Link to="" className="text-gray-700 hover:text-gray-900">Command & Control</Link>
-                    {/* Could add <Link> to link to specific product page */}
-                  </li>
-                  <li className="px-4 py-3">
-                    <Link to="" className="text-gray-700 hover:text-gray-900">Mission Autonomy</Link>
-                    {/* Could add <Link to link to specific product page */}
-                  </li>
-                </ul>
-              )}
-            </li>
-            <li className='cursor-pointer'>
-              <Link to="/air-security">Air Security</Link>
-            </li>
-            <li className='cursor-pointer'>
-              <Link to="/maritime-security">Maritime Security</Link>
-            </li>
-            <li className='cursor-pointer'>
-              <Link to="hiring">We're Hiring</Link> 
-            </li>           
-          </ul>
-
-        </div>
+    <div className="container mx-auto">
+      <div>
+        <ul className='flex justify-between'>
+          <li>
+            <Link to="/">Arcadius WP</Link>
+          </li>
+          <li className="relative group"
+            onMouseEnter={() => setIsOpen(true)}
+          >
+            <Link to="/hive" className="inline-block">H.I.V.E Program</Link>
+            {isOpen && (
+              <ul 
+                className="absolute left-0 mt-1 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none z-50"
+                onMouseLeave={() => setIsOpen(false)}  
+              >
+                <li className="px-4 py-3">
+                  <Link to="/hive/submenu1" className="text-gray-700 hover:text-gray-900">Submenu 1</Link>
+                </li>
+                <li className="px-4 py-3">
+                  <Link to="/hive/submenu2" className="text-gray-700 hover:text-gray-900">Submenu 2</Link>
+                </li>
+              </ul>
+            )}
+          </li>
+          <li>
+            <Link to="/air-security">Air Security</Link>
+          </li>
+          <li>
+            <Link to="/maritime-security">Maritime Security</Link>
+          </li>
+          <li>
+            <Link to="hiring">We're Hiring</Link> 
+          </li>           
+        </ul>
       </div>
-    </>
-  )
+    </div>
+  );
 }
