75cecd5ea2336118164eaefa6374ebb4a7836065
Arthur Fernandez <arthur@snooget.com>
2023-07-22 17:16:42 -0700

    Made dropdown, need to adjust dd size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,45 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
-  const toggleDropdown = () => setIsOpen(!isOpen);
 
   return (
-    <>
-      <div className="container mx-auto"> 
-        <div className="bg-white">
-          <ul className='flex justify-between'>
-            <li className='border-solid border-white cursor-pointer hover:fill-gray-200'>
-              <Link to="/">Arcadius WP</Link>
-            </li>
-            <li className='cursor-pointer'>
-              <Link to="/hive">H.I.V.E Program</Link>
-            </li>
-            <li className='cursor-pointer'>
-              <Link to="/air-security">Air Security</Link>
-            </li>
-            <li className='cursor-pointer'>
-              <Link to="/maritime-security">Maritime Security</Link>
-            </li>
-            <li className='cursor-pointer'>
-              <Link to="hiring">We're Hiring</Link> 
-            </li>           
-          </ul>
-        </div>
+    <div className="container mx-auto">
+      <div>
+        <ul className='flex justify-between'>
+          <li>
+            <Link to="/">Arcadius WP</Link>
+          </li>
+          <li className="relative group"
+            onMouseEnter={() => setIsOpen(true)}
+            onMouseLeave={() => setIsOpen(false)}
+          >
+            <Link to="/hive" className="w-full">H.I.V.E Program</Link>
+            {isOpen && (
+              <div className="pt-2 w-full"> {/* Padding to create bridge and make submenu full width */}
+                <ul className="absolute left-0 w-full rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none z-50">
+                  <li className="px-4 py-3">
+                    <Link to="/hive/submenu1" className="text-gray-700 hover:text-gray-900">Submenu 1</Link>
+                  </li>
+                  <li className="px-4 py-3">
+                    <Link to="/hive/submenu2" className="text-gray-700 hover:text-gray-900">Submenu 2</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </li>
+          <li>
+            <Link to="/air-security">Air Security</Link>
+          </li>
+          <li>
+            <Link to="/maritime-security">Maritime Security</Link>
+          </li>
+          <li>
+            <Link to="hiring">We're Hiring</Link> 
+          </li>           
+        </ul>
       </div>
-    </>
-  )
+    </div>
+  );
 }
