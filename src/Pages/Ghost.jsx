4e00cde9cc29b708ef37807be8692c96f0351e4f
Arthur Fernandez <arthur@snooget.com>
2023-08-10 08:06:09 -0700

    fetched ghost data

--- src/Pages/Ghost.jsx
+++ src/Pages/Ghost.jsx
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Ghost() {
-  return <div>Ghost</div>;
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:1337/api/product-models/4?populate=*"
+        );
+        setProduct(response.data.data);
+      } catch (error) {
+        console.error("Error fetching product data", error);
+      }
+    };
+
+    fetchProduct();
+  }, []);
+
+  return (
+    <>
+      <div className="w-full h-screen bg-black">
+        <video
+          src="https://cdn.sanity.io/files/z5s3oquj/production/2b3482ca376f7a16a9d5a9297b9339e2eb46cdc3.mp4"
+          autoPlay
+          loop
+          muted
+          style={{ objectFit: "cover" }}
+          className="w-full h-full object-cover z-50"
+          allow="autoplay"
+          alt="arcadius systems ghost UAV"
+        ></video>
+      </div>
+    </>
+  );
 }
