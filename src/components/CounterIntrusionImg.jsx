5684c0f68095c623476595434c7fba9e011db068
Arthur Fernandez <arthur@snooget.com>
2023-08-10 06:06:41 -0700

    Added counter intrusion carousel for product pg

--- src/components/CounterIntrusionImg.jsx
+++ src/components/CounterIntrusionImg.jsx
@@ -12,12 +12,42 @@ export default function CounterIntrusionImg() {
   const [activeSlide, setActiveSlide] = useState(0);
   // Above is the state set to zero to initalize at an the first image [0]
 
-  // Below I will make a function that changes the state based onClick of a 'dot'
+  // Below I will make a function that changes the state based on a timer
+  useEffect(() => {
+    // Timer start here:
+    const timer = setInterval(() => {
+      setActiveSlide((prevSlide) => (prevSlide + 1) % img.length);
+    }, 4400);
+
+    // Gotta clear the timer below...first-letter...
+    return () => clearInterval(timer);
+  }, [img.length]);
 
   return (
     <>
-      <div>
-        <div>Something</div>
+      <div className="relative flex flex-col items-center bg-black text-white py-4 border-b border-white">
+        <div className="container text-left mb-10 mt-8 max-w-lg mx-auto px-4 min-h-[300px]">
+          {activeSlide === 1 || activeSlide === 2 ? (
+            <video
+              className="h-[300px]"
+              src={img[activeSlide]}
+              autoPlay
+              muted
+            />
+          ) : (
+            <img src={img[activeSlide]} className="h-full w-full" />
+          )}
+        </div>
+        <div className="flex justify-center mt-4">
+          {img.map((_, index) => (
+            <div
+              key={index}
+              className={`h-2 w-2 mx-1 rounded-full ${
+                index === activeSlide ? "bg-gray-300" : "bg-white"
+              }`}
+            ></div>
+          ))}
+        </div>
       </div>
     </>
   );
