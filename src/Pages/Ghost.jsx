6124f8ab856a45c7de3c98aabe39d8b67bf83aae
Arthur Fernandez <arthur@snooget.com>
2023-08-10 08:27:29 -0700

    Added Ghost Product Page

--- src/Pages/Ghost.jsx
+++ src/Pages/Ghost.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import GhostCarousel from "../components/GhostCarousel";
 
 export default function Ghost() {
   const [product, setProduct] = useState(null);
@@ -32,6 +33,40 @@ export default function Ghost() {
           allow="autoplay"
           alt="arcadius systems ghost UAV"
         ></video>
+      </div>
+      {/*========================================================================================================================================================== */}
+      {/* Below is the mobile version */}
+      <div className="lg:hidden block bg-white text-black">
+        <div className="border-b border-white pb-10">
+          {product ? (
+            <div className="flex flex-col items-center">
+              <div className="container text-left mb-4 mt-8 max-w-lg mx-auto px-4">
+                <GhostCarousel />
+                <div className="pt-10">
+                  <h1 className="text-center pb-10 font-semibold text-xl">
+                    {product.attributes.title}
+                  </h1>
+                  <p className="text-sm font-light pb-10">
+                    {product.attributes.description}
+                  </p>
+                  <p>Price: ${product.attributes.price}</p>
+                </div>
+                {/* Below will be the button to buy now and add to cart...*/}
+                <div className="mt-10 text-center hover:cursor-pointer text-black">
+                  <button className="relative font-thin text-xl">
+                    <div className="">
+                      <div className="bg-gray-300 border border-black rounded-lg py-3 px-10 transition transform duration-200 hover:translate-y-2">
+                        Add to Cart
+                      </div>
+                    </div>
+                  </button>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
       </div>
     </>
   );
---
+++ src/components/GhostCarousel.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+
+export default function () {
+  const img = [
+    "https://cdn.sanity.io/images/z5s3oquj/production/ed7fe06ad850f14bdd5de4c030e278e85dababaa-2000x1080.jpg?w=640&q=75&fit=max",
+    "https://cdn.sanity.io/files/z5s3oquj/production/3e3aabd3f483d52a7a525bfdaee6788556002ef5.mp4",
+    "https://cdn.sanity.io/files/z5s3oquj/production/cbbe090aaa34ee2d6f65bf3ae0ae8c4827f65919.mp4",
+  ];
+
+  const [activeSlide, setActiveSlide] = useState(0);
+
+  useEffect(() => {
+    // Timer start here:
+    const timer = setInterval(() => {
+      setActiveSlide((prevSlide) => (prevSlide + 1) % img.length);
+    }, 4400);
+
+    // Gotta clear the timer below...first-letter...
+    return () => clearInterval(timer);
+  }, [img.length]);
+
+  return (
+    <>
+      <div className="relative flex flex-col items-center bg-black pb-4">
+        <div className="container text-left mb-10 mt-8 max-w-lg mx-auto px-4 min-h-[300px]">
+          {activeSlide === 1 || activeSlide === 2 ? (
+            <video
+              className="h-[300px]"
+              src={img[activeSlide]}
+              autoPlay
+              muted
+            />
+          ) : (
+            <img src={img[activeSlide]} className="h-full w-full" />
+          )}
+        </div>
+        <div className="flex justify-center mt-4">
+          {img.map((_, index) => (
+            <div
+              key={index}
+              className={`h-2 w-2 mx-1 rounded-full ${
+                index === activeSlide ? "bg-gray-300" : "bg-white"
+              }`}
+            ></div>
+          ))}
+        </div>
+      </div>{" "}
+    </>
+  );
+}
