a03f6beac36da61e7df26462301ff49339560ee0
Arthur Fernandez <arthur@snooget.com>
2023-08-16 06:59:50 -0700

    added logic for increase, decrease, and rmv from cart

--- src/components/CartButtons.jsx
+++ src/components/CartButtons.jsx
@@ -1,31 +1,52 @@
 // This file will contain + and - buttons for adding and removing products from the cart
 import React from "react";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 
-export default function CartButtons() {
-  const quantity = 2;
+export default function CartButtons({ id }) {
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+  } = useShoppingCart();
+
+  const quantity = getItemQuantity(id);
+
   return (
     <>
       <div className="flex items-center lg:text-white">
         <div className="flex-1">
           {quantity === 0 ? (
-            <div className="bg-gray-300 border border-black rounded-lg transition transform duration-200 hover:translate-y-2 cursor-pointer">
+            <div
+              className="lg:w-[20%] bg-gray-300 text-black border border-black rounded-lg transition transform duration-200 hover:translate-y-2 cursor-pointer p-2"
+              onClick={() => increaseCartQuantity(id)}
+            >
               + Add to Cart
             </div>
           ) : (
             <div>
               <div className="lg:flex items-center space-x-5 lg:space-x-10">
                 <div className="flex items-center space-x-5 lg:space-x-10">
-                  <button className="bg-blue-200 rounded-md text-4xl px-4 text-black transition transform duration-200 hover:translate-y-2">
+                  <button
+                    className="bg-blue-200 rounded-md text-4xl px-4 text-black transition transform duration-200 hover:translate-y-2"
+                    onClick={() => decreaseCartQuantity(id)}
+                  >
                     -
                   </button>
                   <div className="ml-10 mr-10 text-2xl">
                     <span>{quantity}</span> in cart
                   </div>
-                  <button className="bg-blue-200 rounded-md text-4xl px-4 text-black transition transform duration-200 hover:translate-y-2">
+                  <button
+                    className="bg-blue-200 rounded-md text-4xl px-4 text-black transition transform duration-200 hover:translate-y-2"
+                    onClick={() => increaseCartQuantity(id)}
+                  >
                     +
                   </button>
                 </div>
-                <button className="bg-red-600 rounded-md text-xl p-2 px-4 mt-8 lg:mt-0 text-black transition transform duration-200 hover:translate-y-2">
+                <button
+                  className="bg-red-600 rounded-md text-xl p-2 px-4 mt-8 lg:mt-0 text-black transition transform duration-200 hover:translate-y-2"
+                  onClick={() => removeFromCart(id)}
+                >
                   Remove
                 </button>
               </div>
